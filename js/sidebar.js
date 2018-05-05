d44ea66ae93e1cec25aca18dda7014f62be18556
yanglinjing@gmail.com <yanglinjing@gmail.com>
2018-05-05 10:24:34 +1000

    Slightly changed the sidebar.js

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,6 +1,5 @@
 //retrived from https://www.w3schools.com/howto/howto_js_sidenav.asp
 function changeNav() {
     document.getElementById("mySidenav").classList.toggle('width0');
-    document.getElementById("map").classList.toggle('left330');
     document.getElementById("map").classList.toggle('left0');
 }
